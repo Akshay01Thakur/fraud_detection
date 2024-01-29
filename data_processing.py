a643066dd378c1b1211e23a79960e9735f11c699
Akshay01Thakur <158031110+Akshay01Thakur@users.noreply.github.com>
2024-01-29 21:40:10 +0530

    added a new function sum

--- data_processing.py
+++ data_processing.py
@@ -1,2 +1,8 @@
 print("adding all the dataprocessing code related to fraud detection")
 print("one more change")
+a=5
+b=7
+def sum(a,b):
+    c=a+b
+    return c
+print("added a new function which is not correct")
